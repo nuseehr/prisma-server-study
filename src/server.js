8e5996ae269f59e283e6d3a2c8461e96fc2f3ed9
Lee Sun Hong <nuseehr@naver.com>
2019-07-04 01:33:09 +0900

    update schema

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-console.log('HELLO FUCKIN WORLD!')
+require("dotenv").config();
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({ schema });
+
+server.express.use(logger("dev"));
+
+server.start({ port:PORT }, () => 
+  console.log(`🐸 Server running on http://localhost:${PORT}`)
+);
